646fd62cf276c975003b915acb0b25197da7b4cc
iago bortolon <iago_bortolon@hotmail.com>
2022-05-14 13:47:02 -0300

    api request and component construction with data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,73 @@
 import { ProjectCard } from "./components/ProjectCard";
+import "./app.scss";
+import { useEffect, useState } from "react";
+import { api } from "./services/api";
+
+export type projectProps = {
+  pronac: string;
+  city: string;
+  uf: string;
+  resume: string;
+  approvedValue: number;
+  caughtValue: number;
+  name: string;
+};
 
 function App() {
+  const [projects, setProjects] = useState<projectProps[]>([
+    {
+      pronac: "",
+      city: "",
+      uf: "",
+      resume: "",
+      name: "",
+      approvedValue: 0,
+      caughtValue: 0,
+    },
+  ]);
+
+  useEffect(() => {
+    async function getProjects() {
+      await api
+        .get("/projetos/?limit=10")
+        .then((res) => {
+          const parsedProjects = res.data._embedded.projetos.map(
+            (project: any) => {
+              return {
+                pronac: project.PRONAC,
+                city: project.municipio,
+                uf: project.UF,
+                resume: project.resumo,
+                name: project.nome,
+                approvedValue: project.valor_aprovado,
+                caughtValue: project.valor_captado,
+              };
+            }
+          );
+          setProjects(parsedProjects);
+        })
+        .catch((err) => console.log(err));
+    }
+
+    getProjects();
+  }, []);
+
   return (
     <>
-      <div>
-        <ProjectCard />
+      <div className="page-container">
+        <h1 className="page-title">Projetos do poponente</h1>
+
+        <div className="showbox">
+          <ul>
+            {projects.map((project: projectProps) => {
+              return (
+                <li key={project.pronac}>
+                  <ProjectCard project={project} />
+                </li>
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,33 +1,44 @@
 import "./index.scss";
 import { AiOutlineHeart } from "react-icons/ai";
 import { useState } from "react";
+import { projectProps } from "../../App";
 
-export function ProjectCard() {
+interface ProjectCardProps {
+  project: projectProps;
+}
+
+export function ProjectCard({ project }: ProjectCardProps) {
   const [isClicked, setIsClicked] = useState(false);
 
   return (
     <>
       <div className="container">
-        <header>ROUANET</header>
+        <header>{project.pronac}</header>
         <span className="project-details">
-          <h1>Projeto teste</h1>
-          <p className="city">São Paulo . SP</p>
-          <p className="description">
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum."
+          <h1>{project.name}</h1>
+          <p className="city">
+            {project.city} . {project.uf}
           </p>
+          <p className="description">{project.resume}</p>
         </span>
         <span className="project-budget">
           <h2>
-            Aprovado <strong>R$ 100.000,00</strong>
+            Aprovado
+            <strong>
+              {project.approvedValue.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </strong>
           </h2>
           <h2>
-            Captado <strong>R$ 500.000,00</strong>
+            Captado
+            <strong>
+              {project.caughtValue.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </strong>
           </h2>
         </span>
         <div className="footer">
